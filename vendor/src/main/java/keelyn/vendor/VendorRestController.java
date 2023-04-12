75b1c4582c37070c7154cbe5fb65844fb7177cd0
Keelymac32 <keelyn.mcnamara@trincoll.edu>
2023-04-11 23:50:05 -0400

    updated 4/11

--- vendor/src/main/java/keelyn/vendor/VendorRestController.java
+++ vendor/src/main/java/keelyn/vendor/VendorRestController.java
@@ -1,26 +1,26 @@
 package keelyn.vendor;
 
 import keelyn.vendor.entities.Shoe;
+import org.springframework.http.MediaType;
 
-import org.apache.tomcat.util.http.parser.MediaType;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import jakarta.validation.Valid;
-
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.ArrayList;
+import java.util.*;;
 
-import javax.print.attribute.standard.Media;
+
+
 
 
 @RestController
+@RequestMapping(value = "/vendor" )
 @SpringBootApplication
 public class VendorRestController {
     
@@ -31,13 +31,13 @@ public class VendorRestController {
    //hard coded shoes that will be pulled instead form check out 
     
     
+   
 
-
-    @GetMapping("/getinventory")
+    @GetMapping()
     public String getinventory(){
 
 //Call to the inventory database will return items in the inventory 
-        ArrayList<Shoe> inventory = new ArrayList<Shoe>();
+    ArrayList<Shoe> inventory = new ArrayList<Shoe>();
         inventory.add(new Shoe(1,"Sneaker","Nike Pegasus",99.99));
         inventory.add(new Shoe(2,"Sneaker","Nike Pegasus",109.99)); 
         inventory.add(new Shoe(4, "Cleat", "Adidas Soccer Cleat", 100.00));
@@ -48,14 +48,16 @@ public class VendorRestController {
 
 
 // Method that adds a shoe to the inventory 
-    @PostMapping("/addshoeinventory")
-    public String addShoeInventory(@Valid @RequestBody Shoe shoe){
+    @PostMapping()
+    public Shoe addShoeInventory(@Valid @RequestBody Shoe shoe){
     
 
-
-   return "Added Shoe";
+    return shoe;
 
 }
+
+
+
 
 
     @DeleteMapping("/deleteshoeinventory")

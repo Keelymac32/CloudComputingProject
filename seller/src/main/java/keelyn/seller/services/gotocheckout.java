8968ec39ad42d3ce6b39c9ba9847eb6553054657
Keelymac32 <keelyn.mcnamara@trincoll.edu>
2023-04-24 11:13:08 -0400

    Update gotocheckout.java

--- seller/src/main/java/keelyn/seller/services/gotocheckout.java
+++ seller/src/main/java/keelyn/seller/services/gotocheckout.java
@@ -12,17 +12,26 @@ public class gotocheckout {
     
     public Double gettingTotal(List<Shoe> shoeList) {
         //urls for get and post commands on checkout microservice 
-        String postUrl = "http://localhost:8080/checkout/addcart";
-        String getUrl = "http://localhost:8080/checkout/total";
         
+        String hosturl = System.getProperty("hosturl","localhost");
+        String porturl = System.getProperty("porturl","8081");
+
+        String geturl = "http://" + hosturl + ":" + porturl + "/checkout/total";
         
+        String posturl = "http://" + hosturl + ":" + porturl + "/checkout/addcart";
+       
+
+
+
+
         RestTemplate restTemplate = new RestTemplate();
        
         //I think this is passing list of shoes to the POST function in checkout microservice
-        restTemplate.postForObject(postUrl, shoeList, null) ;
+        restTemplate.postForObject(posturl, shoeList, null) ;
         
         //Getting total calculated in GET function of checkout microservice
-        Double total = restTemplate.getForObject(getUrl, Double.class);
+        Double total = restTemplate.getForObject(geturl, Double.class);
+        
         return total;
     }
 

e7d64891d0d87da1dc9b7c6ed80fe174e9969207
Keelymac32 <keelyn.mcnamara@trincoll.edu>
2023-05-06 23:05:30 -0400

    bash scripts to depoly

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+git clone https://github.com/Keelymac32/CloudComputingProject
+cd CloudComputingProject
+kubectl apply -f kubernetes 
---
+++ deployGKE.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+git clone https://github.com/Keelymac32/CloudComputingProject
+cd CloudComputingProject
+kubectl apply -f kubernetes
+gcloud compute firewall-rules create firewall-for-gke31 --allow tcp:31000
+gcloud compute firewall-rules create firewall-for-gke32 --allow tcp:3100
+gcloud compute firewall-rules create firewall-for-gke311 --allow tcp:31001

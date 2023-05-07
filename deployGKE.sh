435ca3209b07c2fcc1eb31cb55463117a318a901
Keelymac32 <91858712+Keelymac32@users.noreply.github.com>
2023-05-07 16:15:25 -0400

    Update deployGKE.sh

--- deployGKE.sh
+++ deployGKE.sh
@@ -5,4 +5,3 @@ kubectl apply -f kubernetes
 gcloud compute firewall-rules create firewall-for-gke31 --allow tcp:31000
 gcloud compute firewall-rules create firewall-for-gke32 --allow tcp:3100
 gcloud compute firewall-rules create firewall-for-gke311 --allow tcp:31001
-kubectl get all 

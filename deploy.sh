9d5b0cfd811aa1c966e8f75aa3c9d372095bb707
Keelymac32 <keelyn.mcnamara@trincoll.edu>
2023-05-07 13:47:41 -0400

    updated deployment files

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,4 @@
 git clone https://github.com/Keelymac32/CloudComputingProject
 cd CloudComputingProject
 kubectl apply -f kubernetes 
+kubectl get all
--- deployGKE.sh
+++ deployGKE.sh
@@ -5,3 +5,4 @@ kubectl apply -f kubernetes
 gcloud compute firewall-rules create firewall-for-gke31 --allow tcp:31000
 gcloud compute firewall-rules create firewall-for-gke32 --allow tcp:3100
 gcloud compute firewall-rules create firewall-for-gke311 --allow tcp:31001
+kubectl get all 
